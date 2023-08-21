e2c8701474d6635b63bbf7d6a97232ff97df11ef
hichamelg110 <elisejuneaileen@gmail.com>
2023-08-21 10:54:56 -0700

    0-task_1.c

--- 0-task_1.c
+++ 0-task_1.c
@@ -1,64 +1,57 @@
 #include "main.h"
 /**
-  *_printf - print whatever it takes as argument
+  *_printf - function that produces output according to a format
   *Return: int
   *@format: argument
   */
 int _printf(const char *format, ...)
 {
-	va_list ptr;
-	int count = 0, i;
-	char c;
-	char *str;
+    va_list ptr;
+    int count = 0, i;
+    char c;
+    char *str;
 
-	if (format == 0)
-	{
-		putchar('(');
-		putchar('n');
-		putchar('u');
-		putchar('l');
-		putchar('l');
-		putchar(')');
-		putchar('\n');
-		count += 7;
-	}
-	else
-	{
-		va_start(ptr, format);
-		while (*format)
-		{
-			if (*format == '%')
-			{
-				format++;
-				if (*format == 'c')
-				{
-					c = va_arg(ptr, int);
-					putchar(c);
-					count++;
-				}
-				else if (*format == 's')
-				{
-					str = va_arg(ptr, char *);
-					for (i = 0; str[i] != '\0'; i++)
-					{
-						putchar(str[i]);
-						count++;
-					}
-				}
-				else if (*format == '%')
-				{
-					putchar('%');
-					count++;
-				}
-			}
-			else
-			{
-				putchar(*format);
-				count++;
-			}
-			format++;
-		}
-	}
-	va_end(ptr);
-	return (count);
+    if (format == 0)
+    {
+        char *null = "(null)\n";
+        for (i = 0; null[i] != '\0'; i++)
+        {
+            putchar(null[i]);
+            count++;
+        }
+    }
+        else
+        {
+                va_start(ptr, format);
+                while (*format)
+                {
+                        if (*format == '%')
+                        {
+                                format++;
+                                if (*format == 'c')
+                                {
+                                        c = va_arg(ptr, int);
+                                        putchar(c);
+                                        count++;
+                                }
+                                else if (*format == 's')
+                                {
+                                        str = va_arg(ptr, char *);
+                                        for (i = 0; str[i] != '\0'; i++)
+                                        {
+                                                putchar(str[i]);
+                                                count++;
+                                        }
+                                }
+                                else if (*format == '%')
+                                {
+                                        putchar('%');
+                                        count++;
+                                }
+                        }
+                        format++;
+                }
+        }
+        va_end(ptr);
+        return (count);
 }

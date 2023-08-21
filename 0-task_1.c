bea5381e56ca082058782738210fb731a7d63d1c
hichamelg110 <elisejuneaileen@gmail.com>
2023-08-21 16:01:34 -0700

    task0

--- 0-task_1.c
+++ 0-task_1.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _printf - print formatted output to stdout
+ * _printf - function that produces output according to a format
  * @format: format string
  * Return: number of characters printed
  */

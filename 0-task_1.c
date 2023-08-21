82cf9e424aac61583da7d0bd1830a7e33f8c078b
hichamelg110 <elisejuneaileen@gmail.com>
2023-08-21 11:00:01 -0700

    0-task_1.c

--- 0-task_1.c
+++ 0-task_1.c
@@ -6,52 +6,52 @@
   */
 int _printf(const char *format, ...)
 {
-    va_list ptr;
-    int count = 0, i;
-    char c;
-    char *str;
+va_list ptr;
+int count = 0, i;
+char c;
+char *str;
 
-    if (format == 0)
-    {
-        char *null = "(null)\n";
-        for (i = 0; null[i] != '\0'; i++)
-        {
-            putchar(null[i]);
-            count++;
-        }
-    }
-        else
-        {
-                va_start(ptr, format);
-                while (*format)
-                {
-                        if (*format == '%')
-                        {
-                                format++;
-                                if (*format == 'c')
-                                {
-                                        c = va_arg(ptr, int);
-                                        putchar(c);
-                                        count++;
-                                }
-                                else if (*format == 's')
-                                {
-                                        str = va_arg(ptr, char *);
-                                        for (i = 0; str[i] != '\0'; i++)
-                                        {
-                                                putchar(str[i]);
-                                                count++;
-                                        }
-                                }
-                                else if (*format == '%')
-                                {
-                                        putchar('%');
-                                        count++;
-                                }
-                        }
-                        format++;
-                }
-        }
-        va_end(ptr);
-        return (count);
+if (format == 0)
+{
+char *null = "(null)\n";
+for (i = 0; null[i] != '\0'; i++)
+{
+putchar(null[i]);
+count++;
+}
+}
+else
+{
+va_start(ptr, format);
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'c')
+{
+c = va_arg(ptr, int);
+putchar(c);
+count++;
+}
+else if (*format == 's')
+{
+str = va_arg(ptr, char *);
+for (i = 0; str[i] != '\0'; i++)
+{
+putchar(str[i]);
+count++;
+}
+}
+else if (*format == '%')
+{
+putchar('%');
+count++;
+}
+}
+format++;
+}
+}
+va_end(ptr);
+return (count);
 }

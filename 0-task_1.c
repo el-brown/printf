06dacfbf23a14cdd7bcd7711f0205ba2cf2ef4af
el-brown <zakariaayoub366@gmail.com>
2023-08-22 14:28:59 -0700

    0-task_1.c

--- 0-task_1.c
+++ 0-task_1.c
@@ -12,32 +12,52 @@ int _printf(const char *format, ...)
 	char *str;
 	
 	if (format == NULL)
+	{
 		char *null_str = "(null)\n";
 		for (count = 0; null_str[count] != '\0'; count++)
+		{
 			putchar(null_str[count]);
+		}
 		return (count);
+	}
 	va_start(ptr, format);
 	while (*format)
+	{
 		if (*format == '%')
+		{
 			format++;
 			if (*format == 'c')
+			{
 				c = va_arg(ptr, int);
 				putchar(c);
 				count++;
+			}
 			else if (*format == 's')
+			{
 				str = va_arg(ptr, char *);
 				if (str == NULL)
+				{
 					str = "(null)";
+				}
 				for (i = 0; *str != '\0'; str++)
+				{
 					putchar(str[i]);
 					count++;
+				}
+			}
 			else if (*format == '%')
+			{
 				putchar('%');
 				count++;
+			}
+		}
 		else
+		{
 			putchar(*format);
 			count++;
+		}
 		format++;
+	}
 	va_end(ptr);
 	return (count);
 }

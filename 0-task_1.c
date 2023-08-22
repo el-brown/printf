d928360db0aa6de3a93a6300c5f6fa4c1028e884
el-brown <zakariaayoub366@gmail.com>
2023-08-22 14:24:07 -0700

    0-task_1.c

--- 0-task_1.c
+++ 0-task_1.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _printf - function that produces output according to a format
  * @format: format string
@@ -11,55 +10,34 @@ int _printf(const char *format, ...)
 	int count = 0, i;
 	char c;
 	char *str;
+	
 	if (format == NULL)
-	{
 		char *null_str = "(null)\n";
 		for (count = 0; null_str[count] != '\0'; count++)
-		{
 			putchar(null_str[count]);
-		}
 		return (count);
-	}
-
 	va_start(ptr, format);
-
 	while (*format)
-	{
 		if (*format == '%')
-		{
 			format++;
 			if (*format == 'c')
-			{
 				c = va_arg(ptr, int);
 				putchar(c);
 				count++;
-			}
 			else if (*format == 's')
-			{
 				str = va_arg(ptr, char *);
 				if (str == NULL)
-				{
 					str = "(null)";
-				}
 				for (i = 0; *str != '\0'; str++)
-				{
 					putchar(str[i]);
 					count++;
-				}
-			}
 			else if (*format == '%')
-			{
 				putchar('%');
 				count++;
-			}
-		}
 		else
-		{
 			putchar(*format);
 			count++;
-		}
 		format++;
-	}
 	va_end(ptr);
 	return (count);
 }
